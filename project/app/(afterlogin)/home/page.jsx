7690bf257a19c9359809a1f042c7f899187a3ae9
kimtaeyoung <trust00005@naver.com>
2024-01-19 01:25:41 +0900

    home 마무리

--- project/app/(afterlogin)/home/page.jsx
+++ project/app/(afterlogin)/home/page.jsx
@@ -4,6 +4,8 @@ import AllPage from "@/app/components/hot/AllPage";
 import WaitingAnswer from "@/app/components/waitinganswer/WaitingAnswer";
 import styles from "@/app/modules/homeCss/home.module.scss";
 import { PageRendering } from "@/app/zustand/store";
+import HomeWriteButtonLay from "@/app/components/waitinganswer/HomeWriteButtonLay";
+import HomePoint from "@/app/components/waitinganswer/HomePoint";
 
 export default function Home() {
   const statePage = PageRendering((state) => state.activePage);
@@ -17,6 +19,12 @@ export default function Home() {
           </div>
           <div>
             <WaitingAnswer />
+          </div>
+          <div>
+            <HomeWriteButtonLay />
+          </div>
+          <div>
+            <HomePoint />
           </div>
         </>
       ) : statePage === "viewPage" ? (
