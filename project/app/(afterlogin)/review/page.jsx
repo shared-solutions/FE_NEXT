09a5ff6ac97ceedd282a900102011371bd995f16
igaeun <igaeun0112@gmail.com>
2024-01-18 13:32:46 +0900

    #2 Feat: 커뮤니티 페이지 구현

--- project/app/(afterlogin)/review/page.jsx
+++ project/app/(afterlogin)/review/page.jsx
@@ -1,7 +1,32 @@
-export default function Home(){
+'use client'
+
+import styles from '@/app/modules/review.module.scss'
+
+const Home = ()=> {
     return(
-        <div>
-            고민후기
+        <div className={styles.container}>
+            <div className={styles.button_container}>
+                <div className={styles.sorting_button_container}>
+                    <button className={styles.button}>조회순</button>
+                    <button className={styles.button}>최신순</button>
+                </div>
+                <div className={styles.submit_button_container}>
+                    <button className={styles.submit_button}>고민투표 후기 작성하기</button>
+                </div>
+            </div>
+            <div className={styles.scrollable_container}>
+                <div className={styles.review_box}>
+                    <p>후기 1</p>
+                </div>
+                <div className={styles.review_box}>
+                    <p>후기 2</p>
+                </div>
+                <div className={styles.review_box}>
+                    <p>후기 3</p>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Home;
---
+++ project/app/components/reviews/review-items.jsx
@@ -0,0 +1,7 @@
+export default function ReviewItem(data){
+    return (
+        <div>
+            <h1>{data}</h1>
+        </div>
+    )
+}
