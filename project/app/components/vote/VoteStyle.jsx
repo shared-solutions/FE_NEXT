2f5a98b2d8766fd702422ea968c072da05d1b9db
igaeun <igaeun0112@gmail.com>
2024-01-20 03:37:25 +0900

    #6 Feat: 투표 스타일 추가

--- project/app/components/vote/VoteStyle.jsx
+++ project/app/components/vote/VoteStyle.jsx
@@ -1,9 +1,38 @@
 'user client'
+import React, { useState } from 'react' 
 import styles from '@/app/modules/voteCss/votestyle.module.scss';
+import VoteStyleButton from './VoteStyleButton';
 
 const VoteStyle = () => {
+    const [selectedButton, setSelectedButton] = useState('일반');
+
+    const handleButtonClick = (text) => {
+        setSelectedButton(text);
+    };
+
     return(
-        <div></div>
+        <div className={styles.container}>
+            <VoteStyleButton
+                text='일반'
+                selected={selectedButton === '일반'}
+                onClick={() => handleButtonClick('일반')}
+            />
+            <VoteStyleButton
+                text='그래프'
+                selected={selectedButton === '그래프'}
+                onClick={() => handleButtonClick('그래프')}
+            />
+            <VoteStyleButton
+                text='게이지'
+                selected={selectedButton === '게이지'}
+                onClick={() => handleButtonClick('게이지')}
+            />
+            <VoteStyleButton
+                text='카드'
+                selected={selectedButton === '카드'}
+                onClick={() => handleButtonClick('카드')}
+            />
+        </div>
     )
 }
 
