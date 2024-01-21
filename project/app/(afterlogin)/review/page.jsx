52a202f701bc3278208d3703528424599671b4ce
igaeun <igaeun0112@gmail.com>
2024-01-22 00:44:44 +0900

    #2 Refactor: 후기 페이지 폴더 구조 수정

--- project/app/(afterlogin)/review/page.jsx
+++ project/app/(afterlogin)/review/page.jsx
@@ -1,32 +1,19 @@
 'use client'
+import Header from '@/app/components/reviews/Header'
+import ReviewBox from '@/app/components/reviews/ReviewBox'
 
-import styles from '@/app/modules/review.module.scss'
+import styles from '@/app/modules/reviewCss/review.module.scss'
 
-const Home = ()=> {
+export default function Review(){
     return(
         <div className={styles.container}>
-            <div className={styles.button_container}>
-                <div className={styles.sorting_button_container}>
-                    <button className={styles.button}>조회순</button>
-                    <button className={styles.button}>최신순</button>
-                </div>
-                <div className={styles.submit_button_container}>
-                    <button className={styles.submit_button}>고민투표 후기 작성하기</button>
-                </div>
-            </div>
+            <Header />
             <div className={styles.scrollable_container}>
-                <div className={styles.review_box}>
-                    <p>후기 1</p>
-                </div>
-                <div className={styles.review_box}>
-                    <p>후기 2</p>
-                </div>
-                <div className={styles.review_box}>
-                    <p>후기 3</p>
-                </div>
+                <ReviewBox />
+                <ReviewBox />
+                <ReviewBox />
+                <ReviewBox />
             </div>
         </div>
     )
 }
-
-export default Home;
