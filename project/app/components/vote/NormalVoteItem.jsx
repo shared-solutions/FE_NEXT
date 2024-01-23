97e5f4516a78411afc42aa977be7d1ec885c2403
igaeun <igaeun0112@gmail.com>
2024-01-23 18:01:10 +0900

    #6  Feat: 일반 스타일 > 항목 추가/삭제

--- project/app/components/vote/NormalVoteItem.jsx
+++ project/app/components/vote/NormalVoteItem.jsx
@@ -1,5 +1,5 @@
 'user client'
-// import React from 'react'
+import React, { useState } from 'react'
 import styles from '@/app/modules/voteCss/normalvoteitem.module.scss'
 import Image from 'next/image'
 
@@ -8,34 +8,55 @@ import addbutton from '../../public/image/add_button.png'
 import deleteimg from '../../public/image/delete.png'
 
 const NormalVoteItem = () => {
+    const [voteItems, setVoteItems] = useState(['투표 항목 1']); // 초기값 설정
+
+    const handleAddItem = () => {
+        const newItemNumber = voteItems.length + 1;
+        const newItemText = `투표 항목 ${newItemNumber}`;
+        setVoteItems([...voteItems, newItemText]);
+    };
+
+    const handleDeleteItem = (index) => {
+        const updatedItems = [...voteItems];
+        updatedItems.splice(index, 1);
+        setVoteItems(updatedItems);
+    };
+
     return (
         <div className={styles.container}>
             <p>항목</p>
-            <div className={styles.box}>
-                <input className={styles.write_item} type='text' placeholder='투표 항목 1' />
-                <div className={styles.menu_container}>
-                {/* 이미지 버튼 */}
-                    <Image 
-                        src={oneimg}
-                        style={{
-                        width: 18,
-                        height: 18
-                        }}
-                        alt='img/'
+            {voteItems.map((item, index) => (
+                <div className={styles.box} key={index}>
+                    <input 
+                    className={styles.write_item} 
+                    type='text' 
+                    placeholder={item} 
                     />
-                    {/* 삭제 버튼 */}
-                    <Image
-                        src={deleteimg} 
-                        style={{
-                        width: 18,
-                        height: 18
-                        }}
-                        alt='delete/'
-                    />
+                    <div className={styles.menu_container}>
+                    {/* 이미지 버튼 */}
+                        <Image 
+                            src={oneimg}
+                            style={{
+                            width: 18,
+                            height: 18
+                            }}
+                            alt='img/'
+                        />
+                        {/* 삭제 버튼 */}
+                        <Image
+                            src={deleteimg} 
+                            style={{
+                            width: 18,
+                            height: 18
+                            }}
+                            alt='delete/'
+                            onClick={() => handleDeleteItem(index)}
+                        />
+                    </div>
                 </div>
-            </div>
+            ))}
             <div className={styles.add_item}>
-                <button>
+                <button onClick={handleAddItem}>
                     <Image
                         src={addbutton}
                         style={{
