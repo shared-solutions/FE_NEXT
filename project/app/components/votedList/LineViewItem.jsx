09d526e31282d00154a979ca7a36dee66d8d5316
igaeun <igaeun0112@gmail.com>
2024-01-27 01:14:21 +0900

    #2 Comment: 주석 삭제

--- project/app/components/votedList/LineViewItem.jsx
+++ project/app/components/votedList/LineViewItem.jsx
@@ -35,58 +35,6 @@ const LineViewItem = () => {
                     onClick={() => handleBoxClick(index)}
                 />
             ))}
-                {/* <LineBox 
-                    top="1일전" 
-                    title="배고프네요 뭐 먹을까요?" 
-                    content="먹을 것 추천 좀 해주세요 두개 중에 골라주세요 ㅋ" 
-                    goodCount={34} 
-                    messageCount={24}
-                    isSelected={selectedBoxIndex === 0}
-                    onClick={() => onSelectBox(0)}
-                />
-                <LineBox 
-                    top="2일전" 
-                    title="제목1" 
-                    content="내용1" 
-                    goodCount={15} 
-                    messageCount={8}
-                    isSelected={selectedBoxIndex === 1}
-                    onClick={() => onSelectBox(1)}
-                />
-                <LineBox 
-                    top="3일전" 
-                    title="제목2" 
-                    content="내용2" 
-                    goodCount={15} 
-                    messageCount={8}
-                    isSelected={selectedBoxIndex === 2}
-                    onClick={() => onSelectBox(2)}
-                />
-                <LineBox 
-                    top="4일전" 
-                    title="제목3" 
-                    content="내용3" 
-                    goodCount={15} 
-                    messageCount={8}
-                    isSelected={selectedBoxIndex === 3}
-                    onClick={() => onSelectBox(3)}
-                />
-                <LineBox 
-                    top="5일전" 
-                    title="제목4" 
-                    content="내용4" 
-                    goodCount={15} 
-                    messageCount={8}isSelected={selectedBoxIndex === 4}
-                    onClick={() => onSelectBox(4)}
-                />
-                <LineBox 
-                    top="6일전" 
-                    title="제목5" 
-                    content="내용5"
-                    goodCount={15} 
-                    messageCount={8}isSelected={selectedBoxIndex === 5}
-                    onClick={() => onSelectBox(5)}
-                /> */}
             </div>
         </div>
     );
