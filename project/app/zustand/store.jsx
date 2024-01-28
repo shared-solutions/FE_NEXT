b626bc43d52a4fff9ee42c1b966663f0b41cc0c4
kimtaeyoung <trust00005@naver.com>
2024-01-28 22:22:36 +0900

    페이지 완료

--- project/app/zustand/store.jsx
+++ project/app/zustand/store.jsx
@@ -1,1 +1,26 @@
 import { create } from "zustand";
+import hotimg from "@/app/public/image/hotimg.png";
+import waitimg from "@/app/public/image/waitimg.png";
+
+export const PageRendering = create((set) => ({
+  activePage: "originalPage",
+  title: "",
+  img: "",
+  changePage() {
+    set({
+      activePage: "viewPage",
+      title: "지금 가장 핫한 고민투표",
+      img: hotimg,
+    });
+  },
+  changePageWait() {
+    set({
+      activePage: "viewPage",
+      title: "답변을 기다리는 고민들",
+      img: waitimg,
+    });
+  },
+  backPage() {
+    set({ activePage: "originalPage" });
+  },
+}));
