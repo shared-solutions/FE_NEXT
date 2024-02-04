ba70d9e7fa9bed345878becb98ba8f3898ede7ae
Minkyung Ko <komg001234@gmail.com>
2024-02-04 23:24:45 +0900

    #20 Refactor: 코드 수정

--- project/app/components/votedList/VotedModal.jsx
+++ project/app/components/votedList/VotedModal.jsx
@@ -1,79 +1,79 @@
-'user clinet'
-import React, { useState } from 'react';
-import styles from '@/app/modules/votedListCss/votedmodal.module.scss';
-import Image from 'next/image'
+"user clinet";
+import React, { useState } from "react";
+import styles from "@/app/modules/votedListCss/votedmodal.module.scss";
+import Image from "next/image";
 
-import GridViewItem from './GridViewItem'
-import LineViewItem from './LineViewItem'
+import GridViewItem from "./GridViewItem";
+import LineViewItem from "./LineViewItem";
 
-import downimg from '../../public/image/down.png'
-import gridview from '../../public/image/grid_view.png'
-import gridViewGrey from '../../public/image/grid_view_grey.png'
-import lineview from '../../public/image/line_view.png'
-import lineViewGrey from '../../public/image/line_view_grey.png'
+import downimg from "../../public/image/down.png";
+import gridview from "../../public/image/grid_view.png";
+import gridViewGrey from "../../public/image/grid_view_grey.png";
+import lineview from "../../public/image/line_view.png";
+import lineViewGrey from "../../public/image/line_view_grey.png";
 
 const VotedModal = ({ onClose }) => {
-    const [selectedStyle, setSelectedStyle] = useState('grid'); // 기본값을 그리드로 설정
+  const [selectedStyle, setSelectedStyle] = useState("grid"); // 기본값을 그리드로 설정
 
-    const renderViewItem = () => {
-        switch (selectedStyle) {
-            case 'line':
-                return <LineViewItem onClose={onClose}/>; // LineViewItem 컴포넌트를 렌더링
-            default:
-                return <GridViewItem />; // GridViewItem 컴포넌트를 렌더링
-        }
-    };
+  const renderViewItem = () => {
+    switch (selectedStyle) {
+      case "line":
+        return <LineViewItem />; // LineViewItem 컴포넌트를 렌더링
+      default:
+        return <GridViewItem />; // GridViewItem 컴포넌트를 렌더링
+    }
+  };
 
-    return (
-        <div className={styles.modal_Overlay}>
-            <div className={styles.modal_container}>
-                <div className={styles.modal_content}>
-                    {/* 상단 바 고정 */}
-                    <button onClick={onClose} className={styles.closeButton}>
-                        <Image
-                            src={downimg}
-                            style={{
-                                width: 23,
-                                height: 10
-                            }}
-                            alt='down/'
-                        />
-                    </button>
-                    {/* ---- 컨텐츠 시작 ---- */}
-                    <div className={styles.modal_innerContent}>
-                        <div className={styles.header}>
-                            <p>내 투표</p>
-                            <div className={styles.choice_container}>
-                                <Image
-                                    src={selectedStyle === 'grid' ? gridview : gridViewGrey}
-                                    style={{
-                                        width: 16,
-                                        height: 16,
-                                        cursor: 'pointer'
-                                    }}
-                                    onClick={() => setSelectedStyle('grid')}
-                                />
-                                <Image
-                                    src={selectedStyle === 'line' ? lineview : lineViewGrey}
-                                    style={{
-                                        width: 16,
-                                        height: 16,
-                                        marginLeft: 18,
-                                        cursor: 'pointer'
-                                    }}
-                                    onClick={() => setSelectedStyle('line')}
-                                />
-                            </div>
-                        </div>
-                        <div className={styles.box_container}>
-                            {renderViewItem()} {/* 기본 값으로 그리드 스타일의 항목 렌더링 */}
-                        </div>
-                    </div>
-                    {/* ---- 컨텐츠 끝 ---- */}
-                </div>
+  return (
+    <div className={styles.modal_Overlay}>
+      <div className={styles.modal_container}>
+        <div className={styles.modal_content}>
+          {/* 상단 바 고정 */}
+          <button onClick={onClose} className={styles.closeButton}>
+            <Image
+              src={downimg}
+              style={{
+                width: 23,
+                height: 10,
+              }}
+              alt="down/"
+            />
+          </button>
+          {/* ---- 컨텐츠 시작 ---- */}
+          <div className={styles.modal_innerContent}>
+            <div className={styles.header}>
+              <p>내 투표</p>
+              <div className={styles.choice_container}>
+                <Image
+                  src={selectedStyle === "grid" ? gridview : gridViewGrey}
+                  style={{
+                    width: 16,
+                    height: 16,
+                    cursor: "pointer",
+                  }}
+                  onClick={() => setSelectedStyle("grid")}
+                />
+                <Image
+                  src={selectedStyle === "line" ? lineview : lineViewGrey}
+                  style={{
+                    width: 16,
+                    height: 16,
+                    marginLeft: 18,
+                    cursor: "pointer",
+                  }}
+                  onClick={() => setSelectedStyle("line")}
+                />
+              </div>
             </div>
+            <div className={styles.box_container}>
+              {renderViewItem()} {/* 기본 값으로 그리드 스타일의 항목 렌더링 */}
+            </div>
+          </div>
+          {/* ---- 컨텐츠 끝 ---- */}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default VotedModal;
--- project/app/components/writereview/WriteReview.jsx
+++ project/app/components/writereview/WriteReview.jsx
@@ -1,93 +1,51 @@
-'use client'
-import styles from '@/app/modules/writepostCss/writepost.module.scss'
-import WriteReviewHeader from "./WriteReviewHeader"
-import WriteReviewFooter from "./WriteReviewFooter"
-import useSelectedBox from '@/app/zustand/selectionStore'
-import { useEffect, useState } from 'react'
-import Image from "next/image";
-
-import userimg from "@/app/public/image/userimg.png";
-import closebtn from "@/app/public/image/close_round.png";
-import selectimg1 from "@/app/public/image/select1.png";
-import selectimg2 from "@/app/public/image/select2.png";
-import likeimg from "@/app/public/image/like.png";
-import commentimg from "@/app/public/image/comment.png";
+"use client";
+import styles from "@/app/modules/writepostCss/writepost.module.scss";
+import WriteReviewHeader from "./WriteReviewHeader";
+import WriteReviewFooter from "./WriteReviewFooter";
+import useSelectedBox from "@/app/zustand/selectionStore";
+import { useEffect, useState } from "react";
 
 const WriteReview = () => {
-    const { selectedBoxData } = useSelectedBox();
+  const { selectedBoxData } = useSelectedBox();
 
-    useEffect(() => {
-        // Zustand에서 가져온 데이터가 변경될 때마다 UI 업데이트
-        if (selectedBoxData) {
-        // 여기서 가져온 데이터를 UI에 적용하거나 로직을 수행할 수 있습니다.
-        console.log('Selected Box Data:', selectedBoxData);
-        }
-    }, [selectedBoxData]);
-    return (
-        <div>
-          <div className={styles.full_container}>
-            <WriteReviewHeader />
-            <div className={styles.content_footer_container}>
-              <div className={styles.content_container}>
-                <input className={styles.title} type="text" placeholder="제목" />
-                <div className={styles.write_content_container}>
-                  <textarea
-                    className={styles.content}
-                    placeholder="함께 공유하고 싶은 내용을 남겨보세요."
-                  />
-                  <div className={styles.pull_review}>
-                    {/* ---- 여기서 Zustand에서 가져온 데이터를 UI에 표시 시작 ---- */}
-                    {selectedBoxData && (
-                        <div className={styles.box}>
-                          <div className={styles.pull_review_header}>
-                            <div className={styles.header_userinfo}>
-                              <Image
-                                src={userimg}
-                                className={styles.userimg}
-                                alt="유저 이미지"
-                                width={24}
-                                height={24}
-                              />
-                              <div className={styles.nickname}>nickname</div>
-                            </div>
-                            <Image
-                              src={closebtn} 
-                              className={styles.closebtn}
-                              alt="닫힘"
-                              width={24}
-                              height={24}
-                            />
-                          </div>
-                          <div className={styles.pull_review_container}>
-                            <div className={styles.pull_review_title}>{selectedBoxData.title}</div>
-                            <div className={styles.pull_review_content}>{selectedBoxData.content}</div>
-                            <div className={styles.pull_reivew_select}>
-                              <Image src={selectimg1} alt="선택지 1" width={98} height={124} />
-                              <Image src={selectimg2} alt="선택지 2" width={98} height={124} />
-                            </div>
-                    
-                            <div className={styles.footer}>
-                              <div className={styles.like}>
-                                <Image src={likeimg} alt="좋아요" width={15} height={13} /> {selectedBoxData.goodCount}
-                              </div>
-                              <div className={styles.comment}>
-                                <Image src={commentimg} alt="댓글" width={15} height={13} /> {selectedBoxData.messageCount}
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                    )}
-                    {/* ---- 여기서 Zustand에서 가져온 데이터를 UI에 표시 끝 ---- */}
-                  </div>
+  useEffect(() => {
+    // Zustand에서 가져온 데이터가 변경될 때마다 UI 업데이트
+    if (selectedBoxData) {
+      // 여기서 가져온 데이터를 UI에 적용하거나 로직을 수행할 수 있습니다.
+      console.log("Selected Box Data:", selectedBoxData);
+    }
+  }, [selectedBoxData]);
+  return (
+    <div>
+      <div className={styles.container}>
+        <WriteReviewHeader />
+        <div className={styles.content_container}>
+          <input className={styles.title} type="text" placeholder="제목" />
+          <div>
+            <textarea
+              className={styles.content}
+              placeholder="함께 공유하고 싶은 내용을 남겨보세요."
+            />
+            <div>
+              {/* 여기서 Zustand에서 가져온 데이터를 UI에 표시 */}
+              {selectedBoxData && (
+                <div>
+                  <p>Selected Box Data:</p>
+                  <p>{selectedBoxData.top}</p>
+                  <p>{selectedBoxData.title}</p>
+                  <p>{selectedBoxData.content}</p>
+                  <p>{selectedBoxData.goodCount}</p>
+                  <p>{selectedBoxData.messageCount}</p>
                 </div>
-              </div>
-              <div className={styles.write_review_footer_container}>
-                <WriteReviewFooter />
-              </div>
+              )}
             </div>
           </div>
         </div>
-      );
-    };
-    
-    export default WriteReview;
+        <input className={styles.tag} type="text" placeholder="# 태그입력" />
+        <WriteReviewFooter />
+      </div>
+    </div>
+  );
+};
+
+export default WriteReview;
