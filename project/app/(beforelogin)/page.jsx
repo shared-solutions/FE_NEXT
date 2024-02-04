b137117e4c8b3fb78e9d2fa2bf5e3efab34c2e0a
JiminLee <201910825>
2024-02-04 10:46:10 +0900

    2024-02-04

--- project/app/(beforelogin)/page.jsx
+++ project/app/(beforelogin)/page.jsx
@@ -4,20 +4,23 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter()
-  const { data: session, status } = useSession()
+  const router = useRouter();
+  const { data: session, status } = useSession();
 
   useEffect(() => {
-    if(status === "authenticated"){
-      router.replace('/home')
+    if (status === "authenticated") {
+      // 이미 인증된 사용자인 경우
+      router.replace('/home');
+    } else if (status === "loading") {
+      // 세션 로딩 중인 경우, 아무 작업 안 함
     } else {
-      router.replace('/i/login')
+      // 로그인되지 않은 경우
+      router.replace('/i/login');
     }
-  }, [session, status, router])
+  }, [session, status, router]);
   
   return (
-      <div>
-
-      </div>
-  )
+    <div>
+    </div>
+  );
 }
--- project/app/components/footer.jsx
+++ project/app/components/footer.jsx
@@ -2,38 +2,45 @@
 'use client'
 import {useSelectedLayoutSegments } from "next/navigation";
 import Link from "next/link";
-
 import styles from '@/app/modules/footer.module.scss'
 
+import F_home from "../public/image/f_home";
+import F_review from "../public/image/f_comment";
+import F_vote from "../public/image/f_vote";
 const Footer = ()=> {
     const segment = useSelectedLayoutSegments()
+    
     return(
         <div className={styles.container} >
             <div className={styles.category}>
-            <Link href='/home'>
+            <Link href='/home' className={styles.f_btn}>
                 {segment[1] === 'home' ?
-                    <p style={{color:"#FFC700"}}>홈</p> :
-                    <p>홈</p>
+                    <F_home fillColor={"#FFE563"}/>
+                    :
+                    <F_home fillColor={"#B9B9B9"}/>
                 }
+                {segment[1] === 'home' ? <p style={{color:"#FFE563"}}>홈</p> :<p style={{color:"#B9B9B9"}}>홈</p> }
             </Link>
-            <Link href='/vote'>
+            <Link href='/vote' className={styles.f_btn}>
                 {segment[1] === 'vote' ?
-                    <p style={{color:"#FFC700"}}>고민투표</p> :
-                    <p>고민투표</p>
+                    <F_vote fillColor={"#FFE563"}/>
+                         
+                    :
+                    <F_vote fillColor={"#B9B9B9"}/>
                 }
+                {segment[1] === 'vote' ? <p style={{color:"#FFE563"}}>고민투표</p> :<p style={{color:"#B9B9B9"}}>고민투표</p> }
             </Link>
-            <Link href='/review'>
+            <Link href='/review' className={styles.f_btn}>
+                
                 {segment[1] === 'review' ?
-                    <p style={{color:"#FFC700"}}>고민후기</p> :
-                    <p>고민후기</p>
+                    <F_review fillColor={"#FFE563"}/>
+                         
+                    :
+                    <F_review fillColor={"#B9B9B9"}/>
                 }
+                {segment[1] === 'review' ? <p style={{color:"#FFE563"}}>고민후기</p> :<p style={{color:"#B9B9B9"}}>고민후기</p> }
             </Link>
-            <Link href='/mypage'>
-                {segment[1] === 'mypage' ?
-                    <p style={{color:"#FFC700"}}>마이페이지</p> :
-                    <p>마이페이지</p>
-                }
-            </Link>
+            
             </div>
         </div>
     )
