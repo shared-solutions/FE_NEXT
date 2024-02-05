1f848c813d2d58964e01bf2e67e9d41b3c8a813b
Minkyung Ko <komg001234@gmail.com>
2024-02-06 03:38:08 +0900

    #24 Design: 애니메이션 수정

--- project/app/template.jsx
+++ project/app/template.jsx
@@ -8,8 +8,8 @@ export default function Transition({ children }) {
   return (
     <motion.div
       key={path}
-      initial={{ x: 1, y: 10, opacity: 0 }}
-      animate={{ x: 0, y: 0, opacity: 1 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ ease: "linear", duration: 0.5 }}
     >
       {children}
