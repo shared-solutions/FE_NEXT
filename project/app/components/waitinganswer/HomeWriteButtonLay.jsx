253791063c00fa3a8523eb498586d27d97cbd30c
kimtaeyoung <trust00005@naver.com>
2024-02-07 19:47:02 +0900

    경로처리

--- project/app/components/waitinganswer/HomeWriteButtonLay.jsx
+++ project/app/components/waitinganswer/HomeWriteButtonLay.jsx
@@ -1,5 +1,5 @@
 import styles from "@/app/modules/waitCss/writebutton.module.scss";
-
+import Link from "next/link";
 const HomeWriteButtonLay = () => {
   return (
     <div className={styles.container}>
@@ -8,7 +8,9 @@ const HomeWriteButtonLay = () => {
         약 50만명의 고민친구들에게 이야기를 공유해보세요. 그들이 당신의 고민을
         해결해줄 지도 모릅니다!
       </div>
-      <button className={styles.writebtn}>글 작성하기</button>
+      <Link className={styles.link} href="/vote/write">
+        <button className={styles.writebtn}>글 작성하기</button>
+      </Link>
     </div>
   );
 };
