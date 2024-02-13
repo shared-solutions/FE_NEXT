5ae883a53457408f35c9301c80897651086e5767
igaeun <igaeun0112@gmail.com>
2024-02-14 04:18:16 +0900

    #35 글 전체보기 > pollVoteType별 컴포넌트 렌더링 (Gauge ver.)

---
+++ project/app/components/postlist/GaugePostBox.jsx
@@ -0,0 +1,40 @@
+import styles from "@/app/modules/postListCss/gaugePostBox.module.scss";
+import likeimg from "@/app/public/image/like.png";
+import commentimg from "@/app/public/image/comment.png";
+import Image from "next/image";
+
+const GaugePostBox = ({ userimg, nickname, title, content, like, comment, pollTitle }) => {
+  return (
+    <div className={styles.box}>
+      <div className={styles.userinfo}>
+        <Image
+          src={userimg}
+          className={styles.userimg}
+          alt="유저 이미지"
+          width={24}
+          height={24}
+        />
+        <div className={styles.nickname}>{nickname}</div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.content}>{content}</div>
+        <div className={styles.pollTitleContainer}>
+          <div className={styles.pollTitle}>{pollTitle}</div>
+          {/* 퍼센티지 하드코딩 수정해야 함 */}
+          <div className={styles.pollTitlePercentage}>0%</div>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.like}>
+            <Image src={likeimg} alt="좋아요" width={15} height={13} /> {like}
+          </div>
+          <div className={styles.comment}>
+            <Image src={commentimg} alt="댓글" width={15} height={13} /> {comment}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GaugePostBox;
