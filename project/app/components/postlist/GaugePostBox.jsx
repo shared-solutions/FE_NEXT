be1e64677af18d03aa0303ee0cc7a89b59ab9364
igaeun <igaeun0112@gmail.com>
2024-02-15 12:07:40 +0900

    #35 글 전체보기 > Gauge ver. 이미지 추가

--- project/app/components/postlist/GaugePostBox.jsx
+++ project/app/components/postlist/GaugePostBox.jsx
@@ -1,9 +1,18 @@
 import styles from "@/app/modules/postListCss/gaugePostBox.module.scss";
 import likeimg from "@/app/public/image/like.png";
 import commentimg from "@/app/public/image/comment.png";
+import gaugeImg from '@/app/public/image/gauge_img.png';
 import Image from "next/image";
 
-const GaugePostBox = ({ userimg, nickname, title, content, like, comment, pollTitle }) => {
+const GaugePostBox = ({ userimg, nickname, title, content, like, comment, pollTitle, gauge }) => {
+  const gaugeValue = gauge === null ? 0 : gauge;
+  const gaugePercentage = gaugeValue + '%';
+  
+  // linear-gradient로 배경색을 설정
+  const gradientStyle = {
+    background: `linear-gradient(to right, black ${gaugePercentage}, #eeeeee ${gaugePercentage})`
+  };
+
   return (
     <div className={styles.box}>
       <div className={styles.userinfo}>
@@ -19,10 +28,15 @@ const GaugePostBox = ({ userimg, nickname, title, content, like, comment, pollTi
       <div className={styles.container}>
         <div className={styles.title}>{title}</div>
         <div className={styles.content}>{content}</div>
-        <div className={styles.pollTitleContainer}>
-          <div className={styles.pollTitle}>{pollTitle}</div>
-          {/* 퍼센티지 하드코딩 수정해야 함 */}
-          <div className={styles.pollTitlePercentage}>0%</div>
+        <div className={styles.gaugeContainer}>
+          <div className={styles.gaugeImageContainer}>
+            <Image src={gaugeImg} alt="게이지 이미지" width={45} height={45} />
+          </div>
+          <div className={styles.pollTitleContainer} style={gradientStyle}>
+            {/* <div className={styles.pollTitle}>{pollTitle}</div> */}
+            {/* 퍼센티지 하드코딩 수정해야 함 */}
+            <div className={styles.pollTitlePercentage}>{gaugeValue}%</div>
+          </div>
         </div>
         <div className={styles.footer}>
           <div className={styles.like}>
