5714283a6df0d4df90d772f3ffb47e9b41bf8b12
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-02-15 15:35:23 +0900

    인피니티 스크롤

--- project/app/api/api/home.js
+++ project/app/api/api/home.js
@@ -1,11 +1,11 @@
 import api from "./index";
 
 // 지금 가장 핫한 고민투표 조회 api
-export const getBest = async () => {
+export const getBest = async (page) => {
   // 쿼리파라미터로 GET /posts/best?page=0&size=3 이런식으로 사용자가 아래로 스크롤 할 수록 page 값을 +1 해서 진행
   console.log("핫한 고민 투표 조회");
   try {
-    const response = await api.get("/posts/best");
+    const response = await api.get(`/posts/best?page=${page}&size=3`);
     return response.data;
   } catch (error) {
     console.log("에러", error);
--- project/app/components/hot/AllPage.jsx
+++ project/app/components/hot/AllPage.jsx
@@ -1,16 +1,82 @@
 "use client";
+import axios from "axios";
 import Image from "next/image";
 import vector from "@/app/public/image/Vector.png";
 import styles from "@/app/modules/hotCss/allpage.module.scss";
 import AllPageBox from "./AllPageBox";
 import { PageRendering } from "@/app/zustand/store";
-import Link from "next/link";
-import { voteData } from "@/app/DATA/dummyData";
+import { useState, useEffect } from "react";
 
 const AllPage = () => {
   const backPage = PageRendering((state) => state.backPage);
   const title = PageRendering((state) => state.title);
   const img = PageRendering((state) => state.img);
+  const [userData, setUserData] = useState([]);
+  const [page, setPage] = useState(0); // 현재 페이지
+  const [loading, setLoading] = useState(false); // 데이터를 불러오는 중인지 여부
+
+  const authToken = localStorage.getItem("token");
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const url = `https://dev.gomin-chingu.site/posts/best`;
+      const params = {
+        page: page,
+        size: 7,
+      };
+      const response = await axios.get(url, {
+        params: params,
+        headers: {
+          "Content-Type": "application/json",
+          atk: authToken,
+        },
+      });
+
+      if (response.status === 200) {
+        const data = response.data;
+        if (page === 0) {
+          setUserData(data.result.content);
+        } else {
+          setUserData((prevData) => [...prevData, ...data.result.content]);
+        }
+        console.log("글 전체보기 데이터:", data);
+      } else {
+        console.error("Failed to get data:", response);
+      }
+    } catch (error) {
+      console.error("Error", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!loading) {
+        if (
+          window.innerHeight + document.documentElement.scrollTop ===
+          document.documentElement.offsetHeight
+        ) {
+          if (userData.length > 0 && !loading) {
+            setPage((prevPage) => prevPage + 1);
+          }
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [userData, loading]);
+
+  console.log(userData);
+
   return (
     <>
       <Image
@@ -32,19 +98,20 @@ const AllPage = () => {
         />
       </div>
 
-      <div className={styles.linkContainer}>
-        {voteData.map((vote, index) => (
-          <Link className={styles.link} href={`/viewdetail/${vote.reviewId}`}>
-            <AllPageBox
-              key={index}
-              userimg={vote.userimg}
-              nickname={vote.nickname}
-              title={vote.title}
-              content={vote.content}
-              selectImgList={vote.selectImgList}
-            />
-          </Link>
-        ))}
+      <div>
+        {userData &&
+          userData.length > 0 &&
+          userData.map((data, index) => (
+            <div key={index}>
+              <AllPageBox
+                userimg={data.user.image}
+                nickname={data.user.nickname}
+                title={data.title}
+                content={data.content}
+                // selectImgList={data.user.image}
+              />
+            </div>
+          ))}
       </div>
     </>
   );
