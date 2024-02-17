7a61a6da114164ffcbeb1a3a0a576472beb463db
Minkyung Ko <komg001234@gmail.com>
2024-02-17 17:14:17 +0900

    #48 상세보기 페이지 구현

--- project/app/zustand/selectionStore.jsx
+++ project/app/zustand/selectionStore.jsx
@@ -5,6 +5,7 @@ const useSelectedBox = create((set) => ({
     selectedBoxData: null, // 새로운 상태 추가
     setSelectedBox: (index) => set({ selectedBoxIndex: index }),
     setSelectedBoxData: (data) => set({ selectedBoxData: data }), // 새로운 함수 추가
+    clearSelectedBox: () => set({ selectedBoxIndex: null, selectedBoxData: null }), // 초기화 함수
   }));
   
 export default useSelectedBox;
