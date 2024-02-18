78a50c8e989f606f214a1e31dbf133d89169b751
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-02-19 06:19:32 +0900

    후기글

--- project/app/components/reviews/Header.jsx
+++ project/app/components/reviews/Header.jsx
@@ -1,15 +1,37 @@
-'use client'
-import styles from '@/app/modules/reviewCss/button.module.scss'
+"use client";
+import React, { useState } from "react";
+import styles from "@/app/modules/reviewCss/button.module.scss";
 
-const Header = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.sorting_button_container}>
-                <div className={styles.button}>조회순</div>
-                <div className={styles.button}>최신순</div>
-            </div>
+const Header = ({ onButtonClick }) => {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (buttonName) => {
+    setSelectedButton(buttonName);
+    onButtonClick(buttonName);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.sorting_button_container}>
+        <div
+          onClick={() => handleButtonClick("조회순")}
+          className={`${styles.button} ${
+            selectedButton === "조회순" ? styles.selected : ""
+          }`}
+        >
+          조회순
         </div>
-    )
-}
+        <div
+          onClick={() => handleButtonClick("최신순")}
+          className={`${styles.button} ${
+            selectedButton === "최신순" ? styles.selected : ""
+          }`}
+        >
+          최신순
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
