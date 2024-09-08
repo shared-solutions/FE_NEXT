274ee101cbf7429e37059dc35c1b2ef353412078
unknown <201910825>
2024-09-08 19:06:06 +0900

    css 반응형 수정

--- project/app/(afterlogin)/viewdetail/[id]/page.jsx
+++ project/app/(afterlogin)/viewdetail/[id]/page.jsx
@@ -53,25 +53,8 @@ export default function Viewdetail({ params }) {
   }, [postId]);
 
   return (
-    <div className={styles.lay}>
-      {/*{detailData.map((detail, index) => (
-        <Detail
-          key={index}
-          userimg={detail.userimg}
-          username={detail.username}
-          date={detail.date}
-          time={detail.time}
-          title={detail.title}
-          content={detail.content}
-          minititle={detail.minititle}
-          point={detail.point}
-          lefttime={detail.lefttime}
-          selectImgList={detail.selectImgList}
-          viewCount={detail.viewCount}
-          likeCount={detail.likeCount}
-          commentCount={detail.commentCount}
-        />
-      ))}*/}
+    <div>
+      
       <Detail
         key={postId}
         username={detail.nickname}
